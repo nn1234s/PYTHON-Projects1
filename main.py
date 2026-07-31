9bfd7c8576318c6dd973cb2c0474ec2635cd59ea
nn1234s <naitiknanda1235@gmail.com>
2026-07-31 11:14:09 +0530

    main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print ("Hello World!")
+print ("Hello World!!")
 print("i am Code1022w in Github.")
 print("I love coding. My favorite video game is Minecraft")
 print("The result of 8 + 8 is", 8 + 8)
