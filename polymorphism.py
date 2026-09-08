47c9510825458491a717c0031348f8a26c5c086d
nn1234s <naitiknanda1235@gmail.com>
2026-09-08 20:10:53 +0530

    update

---
+++ polymorphism.py
@@ -0,0 +1,11 @@
+class Cricket:
+    def info(self):
+        print("CricKet score Information") #the Cricket version
+class Football:
+    def info(self):
+        print("Foot Ball Score Infor mation") #foot Ball version
+c = Cricket()
+f = Football()
+c.info() #Run cricket ' s info
+f.info() # Runs Foot ball ' s Info. Same method name, diff result
+
---
+++ polymorphism1.py
@@ -0,0 +1,10 @@
+class Cricket:
+    def play(self):
+        print("virat Kohli hits a Six!") #cricket ver of play
+class Football:
+    def play(self):
+        print("Rolando hits a goal!") #football ver of play    
+c = Cricket()
+f = Football()    
+c.play() #Run cricket ' s info
+f.play() #run football
